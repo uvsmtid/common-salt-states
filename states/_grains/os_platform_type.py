0dcb836037e85a343329201d3cde03d4b249f625
Alexey Pakseykin <uvsmtid@gmail.com>
2015-03-19 20:25:34 +0800

    Fix `os_platform_type` grains on CentOS 5.5

--- states/_grains/os_platform_type.py
+++ states/_grains/os_platform_type.py
@@ -13,12 +13,22 @@ def provide_os_platform_type():
     elif platform.system() == 'Linux':
         uname_release = platform.uname()[2]
         # Example string to select from:
-        #   3.18.9-200.fc21.x86_64
-        distrib_string_regex = '^\d*\.\d*\.\d*-\d*\.(\w*)\.(\w*)$'
-        #return uname_release
-        re_match = re.match(distrib_string_regex, uname_release)
+        #   - CentOS 7.0:
+        #       3.18.9-200.fc21.x86_64
+        #   - CentOS 5.5:
+        #       2.6.18-194.el5
+
+        # Try various patterns until the first match.
+        re_match = None
+        for distrib_string_regex in [
+            '^\d*\.\d*\.\d*-\d*\.(\w*)\.\w*$', # CentOS 7.0
+            '^\d*\.\d*\.\d*-\d*\.(\w*)$', # CentOS 5.5
+        ]:
+            re_match = re.match(distrib_string_regex, uname_release)
+            if re_match:
+                break;
+
         platform_id = re_match.group(1)
-        platform_arch = re_match.group(2)
 
         # Fedora
         if platform_id.startswith('fc'):
@@ -28,7 +38,7 @@ def provide_os_platform_type():
         # CentOS
         if platform_id.startswith('el'):
             return { 'os_platform_type': 'rh' + platform_id }
- 
+
     elif platform.system() == 'Windows':
         return { 'os_platform_type': 'win7' }
 
