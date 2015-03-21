c069dd70260e14214854dfbcd1d90928c09b06d8
Alexey Pakseykin <uvsmtid@gmail.com>
2015-03-21 21:39:42 +0800

    Fix: change `assert` => `if` for master activation

--- states/common/bootstrap/bootstrap.dir/modules/steps/deploy/activate_salt_master/rhel7.py
+++ states/common/bootstrap/bootstrap.dir/modules/steps/deploy/activate_salt_master/rhel7.py
@@ -8,7 +8,8 @@ from utils.process_zero import start_service_systemd as start_service
 
 def do(action_context):
 
-    assert(action_context.conf_m.activate_salt_master['is_salt_master'])
+    if not action_context.conf_m.activate_salt_master['is_salt_master']:
+        return
 
     enable_service(action_context.conf_m.activate_salt_master['service_name'])
     start_service(action_context.conf_m.activate_salt_master['service_name'])
