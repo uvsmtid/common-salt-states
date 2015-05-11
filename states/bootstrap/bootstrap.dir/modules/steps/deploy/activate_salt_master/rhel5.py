0902134023e7e1e3d0cb53d9aaf8e0b656413485
Alexey Pakseykin <uvsmtid@gmail.com>
2015-05-12 02:48:20 +0800

    Allow 5 sec startup time for salt during bstrap

--- states/bootstrap/bootstrap.dir/modules/steps/deploy/activate_salt_master/rhel5.py
+++ states/bootstrap/bootstrap.dir/modules/steps/deploy/activate_salt_master/rhel5.py
@@ -3,6 +3,8 @@
 from utils.process_zero import enable_service_initd as enable_service
 from utils.process_zero import start_service_initd as start_service
 
+from utils.set_network import ping_host
+
 ###############################################################################
 #
 
@@ -13,6 +15,13 @@ def do(action_context):
 
     enable_service(action_context.conf_m.activate_salt_master['service_name'])
     start_service(action_context.conf_m.activate_salt_master['service_name'])
+
+    # TODO: Is there anything better than 5 sec delay?
+    # Just a 5 sec delay introduced through `ping`.
+    ping_host(
+        'salt',
+        5,
+    )
 
 ###############################################################################
 # EOF
--- states/bootstrap/bootstrap.dir/modules/steps/deploy/activate_salt_minion/rhel7.py
+++ states/bootstrap/bootstrap.dir/modules/steps/deploy/activate_salt_minion/rhel7.py
@@ -3,6 +3,8 @@
 from utils.process_zero import enable_service_systemd as enable_service
 from utils.process_zero import start_service_systemd as start_service
 
+from utils.set_network import ping_host
+
 ###############################################################################
 #
 
@@ -10,6 +12,13 @@ def do(action_context):
 
     enable_service(action_context.conf_m.activate_salt_minion['service_name'])
     start_service(action_context.conf_m.activate_salt_minion['service_name'])
+
+    # TODO: Is there anything better than 5 sec delay?
+    # Just a 5 sec delay introduced through `ping`.
+    ping_host(
+        'salt',
+        5,
+    )
 
 ###############################################################################
 # EOF
