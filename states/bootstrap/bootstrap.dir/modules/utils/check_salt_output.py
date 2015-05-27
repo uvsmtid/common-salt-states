49f8682a6db9ccc739680ebfee1fd1c3e2b2b0f2
Alexey Pakseykin <uvsmtid@gmail.com>
2015-05-28 00:37:20 +0800

    Add initial usage and docs for script

--- states/bootstrap/bootstrap.dir/modules/utils/check_salt_output.py
+++ states/bootstrap/bootstrap.dir/modules/utils/check_salt_output.py
@@ -1,7 +1,54 @@
 #!/usr/bin/env python
 
-#
+"""
+Usage: %(script_name)s path/to/salt/output/file
 
+At the moment Salt version `2015.5.0` does not return non-zero error code
+consistently and this requies verification of result by parsing its output.
+
+This script:
+*   reads file with JSON output of Salt (use `--out json`);
+*   returns non-zero error code if any of the Salt state fails;
+*   supports various output data structures:
+    *   `highstate` output produced by single host (i.e. using `salt-call ...`);
+    *   `highstate` output produced by multiple hosts (i.e. using `salt '*' ...`);
+    *   `orchestrate` runner's output (`salt state.orchestrate ...`).
+
+Why JSON (and not default YAML)?
+
+In fact, YAML parser is used to load JSON files (JSON is sub-syntax of YAML).
+However, it appears that generated YAML format by Salt fails during parsing
+on some platforms (JSON is has simpler syntax and more stable).
+
+Differences between the Salt outputs:
+
+*   If you use Salt minion config options like `show_jid`, it will corrupt
+    stdout injecting job id into JSON output.
+    The script handles it by removing any line which does not match
+    `^[{}\s]`(only indended JSON output gets through).
+
+*   If `highstate` is used with multipe hosts, each of them will generate
+    its own JSON object (which breaks parser expectations).
+
+    TODO: Two approaches
+    *
+    The script handles it by wrapping file's content into another outter
+    pair of brackets `[]` (making it an array). In addition to this,
+    each objects within the array have to be separated by comma `,`.
+    *
+    The script handles this by loading one object at a time into
+    a list of objects.
+
+*   If `orchestrate` runner is used, the output data structure becomes
+    too complex to deal with assumed schema.
+    The script simply traverses all objects within the data and processes
+    only those which have `result` field (assuming it was state result).
+
+    This is basically used as solution for all outputs universally.
+"""
+
+import os
+import re
 import sys
 import yaml
 import logging
@@ -95,6 +142,11 @@ def check_result(salt_output):
 if __name__ == '__main__':
 
     logging.getLogger().setLevel(0)
+
+    # http://stackoverflow.com/a/15064168/441652
+    if len(sys.argv) < 2:
+        print __doc__ % { 'script_name' : sys.argv[0].split(os.pathsep)[-1] }
+        sys.exit(1)
 
     file_path = sys.argv[1]
 
