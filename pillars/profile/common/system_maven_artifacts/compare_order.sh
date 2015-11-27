32b15382a580f8713cdc71bfcadcf9e40d109fb8
Alexey Pakseykin <uvsmtid@gmail.com>
2015-11-28 00:23:07 +0800

    Make script runnable from any directory

--- pillars/profile/common/system_maven_artifacts/compare_order.sh
+++ pillars/profile/common/system_maven_artifacts/compare_order.sh
@@ -1,18 +1,33 @@
 #/bin/sh
 
-set -e
-set -u
-
 # This is a helper script to generate required and existing order
 # of items inside `artifact_descriptors.sls` file.
 
+set -e
+set -u
+
+# Get directory the script is in.
+SCRIPT_DIR="$( dirname "${0}" )"
+if [ "${SCRIPT_DIR:0:1}" == '/' ]
+then
+    # In case of absolute path, just use script dir.
+    RUNTIME_DIR="$( realpath "${SCRIPT_DIR}" )"
+else
+    # In case of relative path, append current workind dir.
+    RUNTIME_DIR="$( realpath "$( pwd )/${SCRIPT_DIR}" )"
+fi
+
 # Note that duplicates also cause error code in addition to unordered ones.
 # The search is done for strings (keys) containing GROUP_ID:ARTIFACT_ID.
-grep -r '^[^:][^:]*:[^:][^:]*:[[:space:]]*$' artifact_descriptors.sls           > existing.order.txt
-grep -r '^[^:][^:]*:[^:][^:]*:[[:space:]]*$' artifact_descriptors.sls | sort -u > required.order.txt
+grep -r '^[^:][^:]*:[^:][^:]*:[[:space:]]*$' \
+    "${RUNTIME_DIR}/"artifact_descriptors.sls           > \
+    "${RUNTIME_DIR}/"existing.order.txt
+grep -r '^[^:][^:]*:[^:][^:]*:[[:space:]]*$' \
+    "${RUNTIME_DIR}/"artifact_descriptors.sls | sort -u > \
+    "${RUNTIME_DIR}/"required.order.txt
 
 set -e
-diff existing.order.txt required.order.txt 1>&2
+diff "${RUNTIME_DIR}/"existing.order.txt "${RUNTIME_DIR}/"required.order.txt 1>&2
 RET_VAL="$?"
 set +e
 
