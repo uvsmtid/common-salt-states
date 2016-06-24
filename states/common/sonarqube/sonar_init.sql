2e01d686a0a4d01391934d2f80b1c86f7f114206
Alexey Pakseykin <uvsmtid@gmail.com>
2016-06-25 06:15:23 +0800

    Fix MySQL/MariaDB issue with use privileges

--- states/common/sonarqube/sonar_init.sql
+++ states/common/sonarqube/sonar_init.sql
@@ -1,25 +1,65 @@
 
-DROP DATABASE IF EXISTS sonar;
-CREATE DATABASE sonar; 
+-- Flushing privileges is done after each statement
+-- to avoid any chance of this wierd isssue.
+-- See: http://stackoverflow.com/a/17436813/441652
+FLUSH PRIVILEGES;
 
-USE mysql;
+-- TODO: Is it really needed if there is `DROP USER` below?
+-- DELETE FROM mysql.db where USER = 'sonar';
 
-DELETE FROM mysql.db where USER = 'sonar';
+FLUSH PRIVILEGES;
 
-GRANT USAGE ON *.* TO sonar@localhost;
+-- Create user to avoid failure of GRANT statement.
+CREATE USER IF NOT EXISTS 'sonar'@'localhost' IDENTIFIED BY '';
 
+FLUSH PRIVILEGES;
+
+-- Create user to avoid failure of GRANT statement.
+CREATE USER IF NOT EXISTS 'sonar'@'%' IDENTIFIED BY '';
+
+FLUSH PRIVILEGES;
+
+-- Make sure user records exist so that dropping user does not fail.
+-- See: http://stackoverflow.com/a/3241918/441652
+GRANT USAGE ON *.* TO 'sonar'@'localhost';
+
+FLUSH PRIVILEGES;
+
+-- Make sure user records exist so that dropping user does not fail.
+-- See: http://stackoverflow.com/a/3241918/441652
 GRANT USAGE ON *.* TO 'sonar'@'%';
 
+FLUSH PRIVILEGES;
 
 DROP USER sonar@localhost;
 
+FLUSH PRIVILEGES;
+
 DROP USER sonar;
 
-CREATE USER sonar@localhost IDENTIFIED BY '';
-CREATE USER 'sonar'@'%' IDENTIFIED BY ''; 
+FLUSH PRIVILEGES;
 
+DROP DATABASE IF EXISTS sonar;
+
+FLUSH PRIVILEGES;
+
+CREATE DATABASE sonar;
+
+FLUSH PRIVILEGES;
+
+CREATE USER sonar@localhost IDENTIFIED BY '';
+
+FLUSH PRIVILEGES;
+
+CREATE USER 'sonar'@'%' IDENTIFIED BY '';
+
+FLUSH PRIVILEGES;
 
 GRANT ALL PRIVILEGES ON sonar.* TO sonar@localhost IDENTIFIED BY '' WITH GRANT OPTION;
+
+FLUSH PRIVILEGES;
+
 GRANT ALL PRIVILEGES ON sonar.* TO 'sonar'@'%' IDENTIFIED BY '' WITH GRANT OPTION;
+
 FLUSH PRIVILEGES;
 
