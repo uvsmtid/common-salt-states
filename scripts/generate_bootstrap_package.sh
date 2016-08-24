9ba8f24b043ab7a6597945bcdf79cedbb5168084
Alexey Pakseykin <uvsmtid@gmail.com>
2016-08-24 16:46:05 +0800

    Fix pillars ref in generate_bootstrap_package.sh

--- scripts/generate_bootstrap_package.sh
+++ scripts/generate_bootstrap_package.sh
@@ -22,7 +22,7 @@ echo "BOOTSTRAP_TARGET_PROFILE=${BOOTSTRAP_TARGET_PROFILE}" 1>&2
 CHECK_SALT_OUTPUT_SCRIPT="${SCRIPT_DIR_PATH}/check_salt_output.py"
 
 # Retrieve absolute path to repo with `bootstrap_target_profile_pillars`:
-BOOTSTRAP_TARGET_PROFILE_REPO="$(sudo salt-call --out txt pillar.get repo_path_bootstrap_target_profile_pillars | cut -d' ' -f2)"
+BOOTSTRAP_TARGET_PROFILE_REPO="$(sudo salt-call --out txt pillar.get properties:repo_path_bootstrap_target_profile_pillars | cut -d' ' -f2)"
 
 # Checkout required branch within repository.
 cd "${BOOTSTRAP_TARGET_PROFILE_REPO}"
